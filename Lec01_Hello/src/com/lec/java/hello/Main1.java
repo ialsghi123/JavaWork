1dc2c6cc591828637d1488353faa278a0973eaea
ialsghi123 <ialsghi123@naver.com>
2020-04-27 11:03:32 +0900

    커밋연습

--- Lec01_Hello/src/com/lec/java/hello/Main1.java
+++ Lec01_Hello/src/com/lec/java/hello/Main1.java
@@ -6,9 +6,12 @@ public class Main1 {
 
 	public static void main(String[] args) {
 		
+		int a;
 		int[] arr= {10,20,30};
 		for(int i=0; i<3; i++) {
 			System.out.println(arr[i]);
+			
+			
 			
 		}
 	}
