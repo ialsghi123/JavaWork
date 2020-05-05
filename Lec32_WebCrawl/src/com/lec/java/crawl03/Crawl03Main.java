3340cb21b694713210fba80282518d5306bd028f
ialsghi123 <ialsghi123@naver.com>
2020-05-05 21:06:18 +0900

    200505

--- Lec32_WebCrawl/src/com/lec/java/crawl03/Crawl03Main.java
+++ Lec32_WebCrawl/src/com/lec/java/crawl03/Crawl03Main.java
@@ -1,12 +1,36 @@
 package com.lec.java.crawl03;
 
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
+import org.jsoup.Connection.Response;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Crawl03Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Daum 실시간 검색어");
 		
 		//http://www.daum.net
 		//15
+		String url;
+		Response response;
+		Elements elements;
+		Document doc; // DOM
+		
+		url="https://media.daum.net/entertain/ranking/popular/";
+		
+		doc = Jsoup.connect(url).execute().parse();
+		
+		elements=doc.select("#cMain > div:nth-child(1) > div:nth-child(2) > ol.list_ranking > li");
+		
+		for(Element e : elements) {
+			
+			System.out.println(e.text().trim());
+		}
 		
 		System.out.println("\n프로그램 종료");
 
