470537451907a2a4e49c76d126df64c9b15fea7a
ialsghi123 <ialsghi123@naver.com>
2020-05-07 23:21:05 +0900

    200507

---
+++ Lec01_Hello/src/com/lec/java/hello/Test.java
@@ -0,0 +1,17 @@
+package com.lec.java.hello;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("201934-153143");
+		System.out.println("김민호");
+		
+		
+		try{ }
+		catch(ArithmeticException ex) { }
+		catch(Exception ex) { }
+		
+
+	}
+
+}
